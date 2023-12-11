02121918fa6bfd0f53a77876f6a9bb55258a2104
Roy Iganza Kibata <iganzaroy55@gmail.com>
2023-12-11 10:48:37 -0800

    added path_handle

--- path.c
+++ path.c
@@ -1,39 +1,25 @@
 #include "shell.h"
 
-/**
- * get_path - ...
- * @cmd: ...
- */
-/*void get_path(char *cmd)
+int path(void)
 {
-	char *path = getenv("PATH");
+	struct node *h;
 
-	if (path == NULL)
+	struct node *current = h;
+
+	while (current != NULL)
 	{
-		perror("Can't get path\n");
-		exit(EXIT_FAILURE);
+		printf("%s\n", current->dir);
+		current = current->next;
 	}
 
-	char *tkn;
-	char *other = path;
-	int got_cmd = 0;
+	struct node *next;
 
-	while ((tkn = strtok_r(other, ":", &other)) != NULL)
+	while (h != NULL)
 	{
-		char cmd_path[MAX_PATH_LEN];
-
-		snprintf(cmd_path, sizeof(cmd_path), "%s%s", tkn, cmd);
-
-		if (access(cmd_path, X_OK) == 0)
-		{
-			got_cmd = 1;
-			execute_cmd(cmd);
-			break;
-		}
+		next = h->next;
+		free(h->dir);
+		free(h);
+		h = next;
 	}
-
-	if (!got_cmd)
-	{
-		_printme("Cant find command: %s\n", cmd);
-	}
-}*/
+	return (0);
+}
